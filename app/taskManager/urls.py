a6ca3b676afc1c0f1d5ca6da2ab29b8139cdf596
akhng999 <akhng999@gmail.com>
2021-05-23 00:46:35 +0800

    update urls.py

--- app/taskManager/urls.py
+++ app/taskManager/urls.py
@@ -14,6 +14,8 @@
 
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = patterns('',
                        url(r'^$',
@@ -24,4 +26,4 @@ urlpatterns = patterns('',
                                    namespace="taskManager")),
                        url(r'^admin/',
                            include(admin.site.urls)),
-                      )
+                      ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
