6073d31b2edb0967d8f5f8cef24b4d7a264f02d9
akhng999 <akhng999@gmail.com>
2021-05-24 13:00:50 +0800

    fixed url import lib

--- app/taskManager/urls.py
+++ app/taskManager/urls.py
@@ -13,6 +13,7 @@
 #
 
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 
 urlpatterns = patterns('',
